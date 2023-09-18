50725dd7c9b52d273e3d07f77baca94340120e91
Mohamed Magdy <mohammedmagdy6522@gmail.com>
2023-09-18 05:33:51 +0300

    Project number 10 Dad Jokes

---
+++ 10_Dad_Jokes/script.js
@@ -0,0 +1,17 @@
+const joke = document.getElementById('joke')
+const jokeBtn =document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click',getJoke)
+
+getJoke()
+
+async function getJoke(){
+    const config = {
+        headers :{
+            Accept : 'application/json',
+        },
+    }
+    const response = await fetch('https://icanhazdadjoke.com',config)
+    const data =await response.json()
+    joke.innerHTML = data.joke
+}
