685c0b071d64c6bbbb6ca64c0074af05dbe778d9
Mohamed Magdy <mohammedmagdy6522@gmail.com>
2024-01-25 02:24:35 +0200

    Add styling for drag and drop

--- 07_Split_Land/script.js
+++ 07_Split_Land/script.js
@@ -1,9 +1,17 @@
 const leftBox = document.querySelector('.leftBox')
 const rightBox = document.querySelector('.rightBox')
 const container = document.querySelector('.container')
-leftBox.addEventListener('mouseenter',()=>{container.classList.add('hover-left')})
-leftBox.addEventListener('mouseleave',()=>{container.classList.remove('hover-left')})
+leftBox.addEventListener('mouseenter',()=>{
+    container.classList.add('hover-left')
+})
 
-rightBox.addEventListener('mouseenter',()=>{container.classList.add('hover-right')})
-rightBox.addEventListener('mouseleave',()=>{container.classList.remove('hover-right')})
+leftBox.addEventListener('mouseleave',()=>{
+    container.classList.add('hover-left')
+})
 
+rightBox.addEventListener('mouseleave',()=>{
+    container.classList.add('hover-right')
+})
+rightBox.addEventListener('mouseleave',()=>{
+    container.classList.add('hover-right')
+})
